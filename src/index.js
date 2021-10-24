7987e45f229c2ad62ee2d2f4dcb46b987f7d6fb4
Angelina <62725741+angelina-7@users.noreply.github.com>
2021-10-24 16:25:24 +0300

    add views and static config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 
 const app = express();
-const PORT = 5000;
+const {PORT} = require('./constants');
+
+//routes
+//controllers
+//services
+//add db
+//add models
+//authentication
+//authorization
+
+require('./config/expressConfig')(app);
+require('./config/hbsConfig')(app);
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('home');
 });
 
 app.listen(PORT, () => console.log(`The app is running on http://localhost:${PORT}`));
